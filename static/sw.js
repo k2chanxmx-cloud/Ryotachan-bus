459b098a4ceaddf0e1927402c574ab1fde220b01
k2chanxmx-cloud <k2chan.xmx@gmail.com>
2026-08-06 11:01:18 +0900

    Add files via upload

--- static/sw.js
+++ static/sw.js
@@ -1,8 +1,39 @@
-const CACHE='kameido-bus-v1';
-const ASSETS=['/','/static/style.css','/static/app.js','/static/manifest.json'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{
-  if(e.request.url.includes('/api/')) return;
-  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
+const CACHE = 'kameido-bus-v6';
+const ASSETS = [
+  '/',
+  '/static/style.css',
+  '/static/app.js',
+  '/static/manifest.json',
+  '/static/icons/icon-192.png',
+  '/static/icons/icon-512.png',
+  '/static/icons/icon-maskable-192.png',
+  '/static/icons/icon-maskable-512.png',
+  '/static/icons/apple-touch-icon.png',
+  '/static/icons/favicon-32.png'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.url.includes('/api/')) return;
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
 });
